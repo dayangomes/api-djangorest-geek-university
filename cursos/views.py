1dda3552e09031e2e42f8953f23d03c421327695
dayangomes <dayanramos5537@gmail.com>
2023-12-27 22:47:35 -0300

    feat: new routes created using generic views

--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -6,41 +6,25 @@ from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class CursoAPIView(APIView):
-    """
-    API de Cursos da Geek University
-    """
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def get(self, request):
-        cursos = Curso.objects.all()
-        # many=True para retornar uma lista de objetos
-        serializer = CursoSerializer(cursos, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        # data=request.data para pegar os dados enviados pelo usuário
-        serializer = CursoSerializer(data=request.data)
-        # Se não for válido, retorna um erro
-        serializer.is_valid(raise_exception=True)
-        serializer.save()  # Salva os dados no banco de dados
-        return Response({"mensagem": "Curso criado com sucesso!", "dados": serializer.data}, status=status.HTTP_201_CREATED)
+    def create(self, request, *args, **kwargs): # Sobrescrevendo o método create
+        response = super().create(request, *args, **kwargs)
+        return Response({"mensagem": "Curso criado com sucesso!", "dados": response.data}, status=status.HTTP_201_CREATED)
 
 
-class AvaliacaoAPIView(APIView):
-    """
-    API de Avaliações da Geek University
-    """
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
 
-    def get(self, request):
-        avaliacoes = Avaliacao.objects.all()
-        # many=True para retornar uma lista de objetos
-        serializer = AvaliacaoSerializer(avaliacoes, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        # data=request.data para pegar os dados enviados pelo usuário
-        serializer = AvaliacaoSerializer(data=request.data)
-        # Se não for válido, retorna um erro
-        serializer.is_valid(raise_exception=True)
-        serializer.save()  # Salva os dados no banco de dados
-        return Response({"mensagem": "Avaliação criada com sucesso!", "dados": serializer.data}, status=status.HTTP_201_CREATED)
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
